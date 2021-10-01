d9de0153572ef601d77e83daa423bcb1ae7d20b9
Tech-Yugank <70506331+Tech-Yugank@users.noreply.github.com>
2021-10-01 14:18:24 +0530

    Add files via upload

---
+++ yt.py
@@ -0,0 +1,15 @@
+import time
+import pyautogui
+import webbrowser
+import random
+
+number = input("hi")
+sleep_time = random.randint(25, 30)
+
+webbrowser.get('C:/Program Files/Google/Chrome/Application/chrome.exe %s').open('')
+time.sleep(5)
+
+for i in range(number):
+	pyautogui.hotkey('ctrl', 'r')
+	print("Watching for {} time".format(i))
+	time.sleep(sleep_time) 
